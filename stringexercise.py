530755dd4949f63bcfacc36b58b54fb4ee17a544
Jairo <Jairo@Jairo.local>
2019-10-01 11:58:30 -0400

    String -More Practice

--- stringexercise.py
+++ stringexercise.py
@@ -7,20 +7,40 @@ Created on Mon Sep 30 16:30:40 2019
 """
 # This short program read in string and print the first two letter
 # and las last two letter of the string
-word = str(input("please Enter the String : "))
-a = len(word)
-print(a)
-if a >= 4:
-    ll = a - 1
-    ll2 = ll -1
-    print("primera two letter are : " + word[0], word[1])
-    print("Last two letters are : " + word[ll2], word[ll]) 
- 
-else :
-    print ("The Word need to be longer than 4 character ")
+
+def menu():
+    print('********** MENU **********')
+    print('1.- Insert String')
+    print('2.- Exit')
+    print('**************************')
+    print()
     
-    
-    
-    
+opcionmenu = 0
+menu()
+while opcionmenu != 2:    
+    opcionmenu = int(input("Select option: "))
+    if opcionmenu == 1:
+        word = str(input("please Enter the String : "))
+        a = len(word)
+        print('\n')
+        print("Length of the String is : ", a)
+        if a >= 4:
+            print('\n')
+            ll = a - 1
+            ll2 = ll -1
+            print("first two letter are : " + word[0], word[1])
+            print("Last two letters are : " + word[ll2], word[ll])
+            if word[1] == word[ll]:
+                print('\n')
+                print("The seconds letters are the SAME") 
+            else:
+                print('\n')
+                print("Fantastic")
+        else :
+            print ("The Word need to be longer than 4 character ")
+menu()        
+print('\n')
+print("**** Thank you for using the program ****" )          
+
     
     
